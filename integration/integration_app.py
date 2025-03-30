72bf1e518bf84ba77fdaca2c1f826134c1ee6f3d
YuvinNP <yuvinnimnaka1998@gmail.com>
2025-03-30 15:27:25 +0530

    Path Changes to Integration Service

--- integration/integration_app.py
+++ integration/integration_app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/integration')
 def hello_world():  # put application's code here
     return 'Hello World! - From Integration'
 
